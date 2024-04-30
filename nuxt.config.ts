160e3915cafe4475465f4545db8ba6a016041b04
Manoj <manojkarthick1706@gmail.com>
2024-04-30 22:43:50 +0530

    Tailwind config completed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  tailwindcss: {
+    cssPath: ["~/assets/css/tailwind.css", { injectPosition: "first" }],
+    configPath: "tailwind.config",
+    exposeConfig: false,
+    config: {},
+    viewer: true,
+  },
+  colorMode: { classSuffix: "" },
+});
